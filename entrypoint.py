178a389c06e7a77381d93abd73fdf57ee2e6159a
Travis Hammond <dashdeckers@gmail.com>
2019-07-23 13:10:01 +0200

    PyInstaller command

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,7 @@
 from DocumentAnalyzer.main import main
 
+# PyInstaller command:
+# pyinstaller --clean --onefile --noconsole --icon=doc.ico --name DocumentAnalyzer entrypoint.py
+
 if __name__ == '__main__':
 	main()
