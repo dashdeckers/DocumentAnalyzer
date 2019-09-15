a15a074197a249b88e7987c6c477b3b84f7102ae
Travis Hammond <dashdeckers@gmail.com>
2019-09-15 14:30:46 +0200

    Updated deployment flow / instructions / settings

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,27 @@
 from DocumentAnalyzer.main import main
 
-# PyInstaller command:
-# pyinstaller --clean --onefile --noconsole --icon=doc.ico --name DocumentAnalyzer entrypoint.py
+'''
+Building on Windows:
+
+> First use PyInstaller to create an executable file that bundles python:
+- Open cmd
+Win + R
+cmd
+- Go to the project directory
+cd Document-Histograms
+- Execute the PyInstaller command
+pyinstaller --clean --onefile --noconsole --icon=doc.ico --name DocumentAnalyzer entrypoint.py
+- Better to use the specfile in which we have specified the Resource folder
+pyinstaller DocumentAnalyzer.spec
+- Replace the .exe in project directory with the new .exe in the dist folder
+
+> Use Inno Setup to create an installation executable from the app exe:
+- Open the InnoSetup_Script.iss file with Inno Setup and compile
+
+> Use MSI-Wrapper to create an MSI installer from the installation exe:
+- Open MSI-Wrapper and use the configuration file or just do it normally
+
+'''
 
 if __name__ == '__main__':
 	main()
